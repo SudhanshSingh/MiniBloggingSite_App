1600340086ba5938a6ab5429b65b5142485456e6
Anil5520 <anilmahto812@gmail.com>
2022-06-21 15:12:17 +0530

    Api for author and blogs1

--- src/models/blogsModel.js
+++ src/models/blogsModel.js
@@ -1,30 +1,48 @@
 const mongoose = require('mongoose');
 
-const blogsSchema = new mongoose.Schema( {
-  title: {
-    type: String, 
-    required: true
-},
-    body:{
-        type: String, 
+const blogsSchema = new mongoose.Schema({
+    title: {
+        type: String,
         required: true
-    }, 
+    },
+    body: {
+        type: String,
+        required: true
+    },
     authorId: {
-        type: String,       
+        type: String,
         required: true
     },
     tags: {
         type: [String]
     },
-    category:{
+    category: {
         type: String,
         required: true
     },
-    subcategory:
+    subcategory: {
+        type: [String]
+    },
 
+    deletedAt: {
+        type: Date,
+        default: null,
+    },
+
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+
+    publishedAt: {
+        type: Date
+    },
+
+    isPublished: {
+        type: Boolean,
+        default: false
+    }
 
 }, { timestamps: true });
 
-module.exports = mongoose.model('Blog', blogsSchema) 
-
-
+module.exports = mongoose.model('Blog', blogsSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
 
+const AuthController= require("../controllers/authorcontroller")
+const BlogsController= require("../controllers/blogscontroller")
+
+
+router.post("/authors", AuthController.createAuthor )
+router.post("/blogs", BlogsController.createBlogs )
+
+module.exports = router; 
